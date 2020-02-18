0d406413bbaf3e7dab95a61e117c8daef72a6357
Jean Carlo Hilger <hilgerjeancarlo@gmail.com>
2020-02-18 17:54:22 -0300

    removes card__action wrong class

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,9 +5,9 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col col-xs-12 col-md-7 col-xl-5">
-                <div class="card__shadow">
-                    <div class="card-body">
-                        
+                <div class="">
+                    <div class="">
+
                         <div class="media">
                             <img src="{{ asset('/img/logo_uffs.png') }}" class="img-fluid" alt="logo_uffs">
                         </div>
@@ -15,17 +15,17 @@
                         <div class="input-group mb-3">
                             <input type="text" class="card__input" placeholder="IdUFFS">
                         </div>
-                        
+
                         <div class="input-group mt-4">
                             <input type="password" class="card__input" placeholder="Senha">
                         </div>
-                        
+
                         <button type="button" class="card__btn btn-primary">ENTRAR</button>
-                        
-                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="card__action text-primary"> Não sabe seu IdUFFS? </a>
-                        
-                        <a href="#" target="_blank" class="card__action text-primary mx-2"> Esqueceu seu senha? </a>
-                        
+
+                        <a href="https://id.uffs.edu.br/id/XUI/?realm=/#forgotUsername/" target="_blank" class="text-primary"> Não sabe seu IdUFFS? </a>
+
+                        <a href="#" target="_blank" class="text-primary mx-2"> Esqueceu seu senha? </a>
+
                     </div>
                 </div>
             </div>
